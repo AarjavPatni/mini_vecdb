fe72d411b441e2b0163dab02f60d47dcd492589d
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2025-05-30 05:08:04 -0400

    feat(vector_db): add error handling and improve store function

--- src/vector_db.rs
+++ src/vector_db.rs
@@ -11,11 +11,29 @@ fn distance(e1: &Embedding, e2: &Embedding) -> f32 {
 
 pub fn search(db: VectorDB, query: Embedding, k: usize) -> Result<Vec<Embedding>, String> {
     // goal: find the top-k results.
+    // exit if db len < 10
+    // get first 10 ele of db, check distance with query, add to array as (embedding, distance)
+    // sort array using distance element
+    // for every next ele, insert before the first element compared to which the distance is
+    // greater
+
+    if db.len() < k {
+        return Err(format!("Size of the database cannot be less than {}", k));
+    }
+
+    let mut top_k: [Embedding; 10];
+    let keys = db.keys();
+
     todo!()
 }
 
-pub fn store(db: VectorDB, embedding: Embedding) -> Result<(), String> {
+pub fn store(db: &mut VectorDB, url: String, embedding: Embedding) -> Result<String, String> {
     // goal: push embedding into db
-    todo!()
+    // the insert fn returns None is the key doesn't exist, and the old value if it does
+
+    match db.insert(url, embedding) {
+        None => Ok("Key inserted".to_string()),
+        Some(i) => Ok(format!("Key replaced. Previous value: {}", i).to_string()),
+    }
 }
 
