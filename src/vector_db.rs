e3d29e7bfe1d42765f357876d6083763e583f024
Aarjav Patni <68058926+AarjavPatni@users.noreply.github.com>
2025-05-30 05:51:42 -0400

    feat(vector_db): improve search function to return top-k embeddings with distances

--- src/vector_db.rs
+++ src/vector_db.rs
@@ -1,18 +1,18 @@
-use std::{collections::HashMap, error::Error};
+use std::{collections::HashMap};
 
-use nalgebra::SVector;
+use nalgebra::{SVector};
 
-type Embedding = SVector<f32, 1024>;
+type Embedding = SVector<f32, 256>;
 type VectorDB = HashMap<String, Embedding>;
 
-fn distance(e1: &Embedding, e2: &Embedding) -> f32 {
+fn similarity(e1: &Embedding, e2: &Embedding) -> f32 {
     e1.dot(e2) / (e1.norm() * e2.norm())
 }
 
-pub fn search(db: VectorDB, query: Embedding, k: usize) -> Result<Vec<Embedding>, String> {
+pub fn search(db: &VectorDB, query: Embedding, k: usize) -> Result<Vec<(Embedding, f32)>, String> {
     // goal: find the top-k results.
-    // exit if db len < 10
-    // get first 10 ele of db, check distance with query, add to array as (embedding, distance)
+    // exit if db len < k
+    // get first k ele of db, check distance with query, add to array as (embedding, distance)
     // sort array using distance element
     // for every next ele, insert before the first element compared to which the distance is
     // greater
@@ -21,10 +21,19 @@ pub fn search(db: VectorDB, query: Embedding, k: usize) -> Result<Vec<Embedding>
         return Err(format!("Size of the database cannot be less than {}", k));
     }
 
-    let mut top_k: [Embedding; 10];
-    let keys = db.keys();
+    let mut top_k: Vec<(Embedding, f32)> = vec![];
+    let keys_vec: Vec<String> = db.keys().cloned().collect();
 
-    todo!()
+    for i in 0..keys_vec.len() {
+        let e1 = db.get(&keys_vec[i]).unwrap().clone();
+        top_k.push((e1, similarity(&e1, &query)));
+        top_k.sort_by(|a, b| a.1.total_cmp(&b.1));
+        if top_k.len() > k {
+            top_k.pop();
+        }
+    }
+
+    Ok(top_k)
 }
 
 pub fn store(db: &mut VectorDB, url: String, embedding: Embedding) -> Result<String, String> {
